43c2071cd5707a1a1c5800fa9b064db24e787366
Anton <anton@adc.sh>
2019-03-31 20:06:48 +0300

    querystring

--- types-v8/index.d.ts
+++ types-v8/index.d.ts
@@ -916,25 +916,6 @@ declare module "buffer" {
   export { BuffType as Buffer, SlowBuffType as SlowBuffer };
 }
 
-declare module "querystring" {
-  export interface StringifyOptions {
-      encodeURIComponent?: Function;
-  }
-
-  export interface ParseOptions {
-      maxKeys?: number;
-      decodeURIComponent?: Function;
-  }
-
-  interface ParsedUrlQuery { [key: string]: string | string[] | undefined; }
-
-  export function stringify<T>(obj: T, sep?: string, eq?: string, options?: StringifyOptions): string;
-  export function parse(str: string, sep?: string, eq?: string, options?: ParseOptions): ParsedUrlQuery;
-  export function parse<T extends {}>(str: string, sep?: string, eq?: string, options?: ParseOptions): T;
-  export function escape(str: string): string;
-  export function unescape(str: string): string;
-}
-
 declare module "events" {
   class internal extends NodeJS.EventEmitter { }
 
