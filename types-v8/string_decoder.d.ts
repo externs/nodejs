df88d44aec76db36b5c9d6817eaf8373d4cb4fd9
Anton <anton@adc.sh>
2019-04-01 08:34:02 +0300

    rest

---
+++ types-v8/string_decoder.d.ts
@@ -0,0 +1,11 @@
+/// <reference path="./buffer.d.ts" />
+
+declare module "string_decoder" {
+  export interface NodeStringDecoder {
+      write(buffer: Buffer): string;
+      end(buffer?: Buffer): string;
+  }
+  export var StringDecoder: {
+      new(encoding?: string): NodeStringDecoder;
+  };
+}
---
+++ types-v8/tty.d.ts
@@ -0,0 +1,17 @@
+/// <reference path="./net.d.ts" />
+
+declare module "tty" {
+  import * as net from "net";
+
+  export function isatty(fd: number): boolean;
+  export class ReadStream extends net.Socket {
+      isRaw: boolean;
+      setRawMode(mode: boolean): void;
+      isTTY: boolean;
+  }
+  export class WriteStream extends net.Socket {
+      columns: number;
+      rows: number;
+      isTTY: boolean;
+  }
+}
